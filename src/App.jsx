4e10941c6517eaa3a60237a72893deb9f321c79d
Deyse Aiala <deyseaiala23@gmail.com>
2024-04-11 20:21:58 -0300

    adicao de props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import CardTrilha from "./components/CardTrilha"
 
 function App() {
+
+  let listaTrilhas = [{
+    nomeTrilha: "Trilha da Costa da Lagoa",
+    cidade: "Florianópolis",
+    estado: "SC",
+    duraçao: 120,
+    trajeto: 4,
+    dificuldade: "Iniciante",
+    tipo: "caminhada/trekking",
+    nomeUsuario: "Deyse Aiala",
+    urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  }]
   
 
   return (
     <>
-      <CardTrilha></CardTrilha>
-        
+      {listaTrilhas.map((trilha,index) => (
+      <CardTrilha dadosTrilha={trilha} key={index} />  
+      ))
+      
+      } 
     </>
   )
 }
--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,9 +1,39 @@
+import * as PropTypes from 'prop-types'
 
-function CardTrilha(){
+function CardTrilha({dadosTrilha}){
 
     return(
-        <h1>componente</h1>
+       
+        <div>
+            <div className="imagemTrilha">
+                <img src={dadosTrilha.urlImagem} alt="imagem da trilha" />
+            </div>
+            <div className="infoTrilha"> 
+                <h1>{dadosTrilha.nomeTrilha}</h1>
+                <span>{dadosTrilha.cidade}/{dadosTrilha.estado}</span>
+                <h3>Por {dadosTrilha.nomeUsuario}</h3>
+                <span>Duração: {dadosTrilha.duracao} min</span>
+                <span>Trajeto: {dadosTrilha.trajeto} km</span>
+                <h6>{dadosTrilha.dificuldade}</h6>
+            </div>
+        </div>
+        
+        
     )
+}
+
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.exact({
+        nomeTrilha: PropTypes.string,
+        cidade: PropTypes.string,
+        estado: PropTypes.string,
+        duracao: PropTypes.number,
+        trajeto: PropTypes.number,
+        dificuldade: PropTypes.string,
+        tipo: PropTypes.string,
+        nomeUsuario: PropTypes.string,
+        urlImagem: PropTypes.string
+    })
 }
 
 export default CardTrilha;
